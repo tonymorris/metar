814be3484ccd88677f85cc0c86739a622c6979c7
Tony Morris <tmorris@tmorris.net>
2018-10-25 19:00:58 +1000

    BOM

--- src/Data/Aviation/Metar.hs
+++ src/Data/Aviation/Metar.hs
@@ -12,80 +12,77 @@ import Prelude
 import Text.HTML.TagSoup.Tree
 import Text.HTML.TagSoup
 
-request ::
-  String
-  -> Request String
-request yxxx =
-  let reqBody =
-        "keyword=" <> yxxx <> "&type=search&page=TAF"
-      in  setHeaders
-        (
-          setRequestBody
-            (
-              mkRequest
-                POST
-                (URI "http" (Just (URIAuth "" "www.bom.gov.au" "")) "/aviation/php/process.php" "" "")
-            )
-            ("application/x-www-form-urlencoded", reqBody)
-        )
-        [
-          Header HdrHost                        "www.bom.gov.au"
-        , Header HdrUserAgent                   "tonymorris/metar"
-        , Header HdrAccept                      "*/*"
-        , Header HdrAcceptLanguage              "en-US,en;q=0.5"
-        , Header HdrAcceptEncoding              "text/html"
-        , Header HdrReferer                     "http://www.bom.gov.au/aviation/forecasts/taf/"
-        , Header HdrConnection                  "keep-alive"
-        , Header HdrContentType                 "application/x-www-form-urlencoded"
-        , Header HdrContentLength               (show (length reqBody))
-        , Header HdrCookie                      "check=ok; bom_meteye_windspeed_units_knots=yes"
-        , Header HdrPragma                      "no-cache"
-        , Header HdrCacheControl                "no-cache"
-        , Header (HdrCustom "DNT")              "1"
-        , Header (HdrCustom "X-Requested-With") "XMLHttpRequest"
-        ]
-
-data TAFResponse =
-  TAFResponse
+data BOMTAFResponse =
+  BOMTAFResponse
     String -- title
     [String] -- TAF
     [String] -- METAR
   deriving (Eq, Ord, Show)
 
-mkTAFResponse ::
-  [TagTree String]
-  -> Maybe TAFResponse
-mkTAFResponse (TagBranch "h3" [] [TagLeaf (TagText title)] : TagBranch "p" [("class","product")] tafs : TagBranch "p" [("class","product")] metars:_) =
-  let tagTexts q =
-        q >>= \r ->
-          case r of
-            TagLeaf (TagText v) ->
-              [v]
-            _ ->
-              []
-  in  Just (TAFResponse title (tagTexts tafs) (tagTexts metars))
-mkTAFResponse _ =
-  Nothing
-
-data TAFResult =
+data TAFResult a =
   ConnErrorResult ConnError
   | ParseErrorResult
-  | TAFResult TAFResponse
+  | TAFResult a
   deriving (Eq, Show)
 
-respTAF ::
-  Response String
-  -> Maybe TAFResponse
-respTAF =
-  mkTAFResponse . parseTree . rspBody
-
-getTAF ::
+getBOMTAF ::
   String
-  -> IO TAFResult
-getTAF =
-  let withResult ::
+  -> IO (TAFResult BOMTAFResponse)
+getBOMTAF =
+  let mkTAFResponse ::
+        [TagTree String]
+        -> Maybe BOMTAFResponse
+      mkTAFResponse (TagBranch "h3" [] [TagLeaf (TagText title)] : TagBranch "p" [("class","product")] tafs : TagBranch "p" [("class","product")] metars:_) =
+        let tagTexts q =
+              q >>= \r ->
+                case r of
+                  TagLeaf (TagText v) ->
+                    [v]
+                  _ ->
+                    []
+        in  Just (BOMTAFResponse title (tagTexts tafs) (tagTexts metars))
+      mkTAFResponse _ =
+        Nothing
+      request ::
+        String
+        -> Request String
+      request yxxx =
+        let reqBody =
+              "keyword=" <> yxxx <> "&type=search&page=TAF"
+            in  setHeaders
+              (
+                setRequestBody
+                  (
+                    mkRequest
+                      POST
+                      (URI "http" (Just (URIAuth "" "www.bom.gov.au" "")) "/aviation/php/process.php" "" "")
+                  )
+                  ("application/x-www-form-urlencoded", reqBody)
+              )
+              [
+                Header HdrHost                        "www.bom.gov.au"
+              , Header HdrUserAgent                   "tonymorris/metar"
+              , Header HdrAccept                      "*/*"
+              , Header HdrAcceptLanguage              "en-US,en;q=0.5"
+              , Header HdrAcceptEncoding              "text/html"
+              , Header HdrReferer                     "http://www.bom.gov.au/aviation/forecasts/taf/"
+              , Header HdrConnection                  "keep-alive"
+              , Header HdrContentType                 "application/x-www-form-urlencoded"
+              , Header HdrContentLength               (show (length reqBody))
+              , Header HdrCookie                      "check=ok; bom_meteye_windspeed_units_knots=yes"
+              , Header HdrPragma                      "no-cache"
+              , Header HdrCacheControl                "no-cache"
+              , Header (HdrCustom "DNT")              "1"
+              , Header (HdrCustom "X-Requested-With") "XMLHttpRequest"
+              ]
+      respTAF ::
+        Response String
+        -> Maybe BOMTAFResponse
+      respTAF =
+        mkTAFResponse . parseTree . rspBody
+      withResult ::
         Either ConnError (Response String)
-        -> TAFResult
+        -> TAFResult BOMTAFResponse
       withResult (Left e) =
         ConnErrorResult e
       withResult (Right s) =
@@ -95,3 +92,5 @@ getTAF =
           Just z ->
             TAFResult z
   in  fmap withResult . simpleHTTP . request
+
+-- http://tgftp.nws.noaa.gov/data/observations/metar/stations/ANAU.TXT
